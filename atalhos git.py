aea6ca72fca79d9a95cce957d85e87d9a0ec925e
Adilson Claro <estudosimformatica@gmail.com>
2022-04-09 11:15:06 -0300

    primeiro atalhos git.py

---
+++ atalhos git.py
@@ -0,0 +1,11 @@
+#git log para print
+# git commit -m "nome do arquivo" para salvar desse jeito
+#git add nome do arquivo para deixar printavel
+#git status
+#git ignore para nao saberem as mudancas
+#arquivo criado .gitignore ignore.py
+#git add . e para todos
+#
+#
+#
+#
